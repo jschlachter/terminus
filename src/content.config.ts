5db7f2e15bea7c35b16a1c58c89636ebb3a93002
Jeremy Schlachter <jeremy.schlachter@uhc.com>
2026-07-20 15:59:17 -0500

    Remove old dependencies

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,7 @@
 import { defineCollection, z } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
 import { glob } from 'astro/loaders';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 	events: defineCollection({
 		loader: glob({ pattern: '*/*/*.md', base: './src/events' }),
 		schema: z.object({
