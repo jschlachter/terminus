89485520c5427733faad4b54217f4a2fcf67e007
Jeremy Schlachter <jeremy.schlachter@west94.org>
2026-07-21 08:20:50 -0500

    Add nodejs adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite"
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [],
-	vite: {
-		plugins: [tailwindcss()]
-	}
+  integrations: [],
+
+  vite: {
+      plugins: [tailwindcss()]
+	},
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
